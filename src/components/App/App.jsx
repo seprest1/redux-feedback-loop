307a58ea8c7c8002b69c2c94f3e0dd35e69aecfc
Sarah Preston <seprest@gmail.com>
2022-09-23 16:28:18 -0500

    imported and tested all components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+//components
+import Admin from '../Admin/Admin';
+import Comments from '../Comments/Comments';
+import Feeling from '../Feeling/Feeling';
+import Header from '../Header/Header';
+import Review from '../Review/Review';
+import Success from '../Success/Success';
+import Support from '../Support/Support';
+import Understanding from '../Understanding/Understanding';
+
 function App() {
 
-  return (
+  return ( //in order of loading for user
     <div className='App'>
-     
+      <Header/>
+      <Feeling/>
+      <Understanding/>
+      <Support/>
+      <Comments/>
+      <Review/>
+      <Success/>
+      <Admin/>
     </div>
   );
 }
