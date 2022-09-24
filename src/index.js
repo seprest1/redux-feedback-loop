c6008d7e6ce650ffb2d46bf7babc5c66f3498b01
Sarah Preston <seprest@gmail.com>
2022-09-23 20:11:21 -0500

    created redux and store, with initial reducer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,36 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducers
+const feedback = (state = {}, action) => {
+    switch(action.type){
+        case('SET_FEELINGS'):
+            return {...state, feeling: action.payload};
+        case('SET_UNDERSTANDING'):
+            return {...state, understanding: action.payload};
+        case('SET_SUPPORT'):
+            return {...state, support: action.payload};
+        case('ADD_COMMENTS'):
+            return {...state, comments: action.payload};
+        case('CLEAR_FEEDBACK'):
+            return {};
+        default:
+            return state;
+    }
+}
+
+// Store
+const storeInstance = createStore(
+    combineReducers({
+       feedback
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
